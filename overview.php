6941b234a95bc1b38023a12b3de62d5468cf4f38
MohammedAB <albay.m@hotmail.com>
2024-01-17 16:53:20 +0100

    Add more data from the collection to display in foreach loop & Add header section

--- overview.php
+++ overview.php
@@ -8,14 +8,31 @@
     <title>Goodcard - track your collection of Pokémon cards</title>
 </head>
 <body>
-
-<h1>Goodcard - track your collection of Pokémon cards</h1>
+<header>
+    <nav>
+        <ul>
+            <li>Overview</li>
+            <li></li>
+        </ul>
+    </nav>
+</header>
+<main>
+<h1>Goodcard - Track your collection of Pokémon cards</h1>
 
 <ul>
     <?php foreach ($cardsData as $card) : ?>
-        <li><?= $card['Name'] ?></li>
+        <li>
+            <h2><?= $card['Name'] ?></h2>
+            <p><?= $card['Rarity'] ?></p>
+            <p><?= $card['Set_name'] ?></p>
+            <p><?= $card['Card_type'] ?></p>
+            <p><?= $card['Release_year'] ?></p>
+            <p><?= $card['Card_condition'] ?></p>
+            <p><?= $card['Notes'] ?></p>
+            <img src="<?php      ?>" alt="Image of the pokémon" width="100" height="100">
+        </li>
     <?php endforeach; ?>
 </ul>
-
+</main>
 </body>
 </html>
